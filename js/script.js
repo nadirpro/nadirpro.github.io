ef11182141926626d37680393c1a2ea18bdcef8b
nadirpro <46989910+nadirpro@users.noreply.github.com>
2019-01-27 23:18:25 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,6 +11,6 @@ function getAge(dateString) {
 function plural(n,str1,str2,str5){return n + ' ' + ((((n%10)==1)&&((n%100)!=11))?(str1):(((((n%10)>=2)&&((n%10)<=4))&&(((n%100)<10)||((n%100)>=20)))?(str2):(str5)))}
 
 window.onload = function() {
-   var age=getAge("03/01/1998")ж
+   var age=getAge("03/01/1998");
    document.getElementById('age').innerHTML=age+' '+plural(age, 'год', 'года', 'лет')+', Тюмень';
 };
